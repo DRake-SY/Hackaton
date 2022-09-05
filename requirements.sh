03232d0b03890f8d067a139cd128655048910ba0
Roro0821 <romane.beghin@outlook.fr>
2022-09-05 23:24:44 +0200

    installation de sratoolkit

---
+++ requirements.sh
@@ -0,0 +1,5 @@
+wget https://ftp-trace.ncbi.nlm.nih.gov/sra/sdk/current/sratoolkit.current-ubuntu64.tar.gz
+tar -zxvf sratoolkit.current-ubuntu64.tar.gz 
+sratoolkit.3.0.0-ubuntu64/bin/vdb-config -i
+
+
